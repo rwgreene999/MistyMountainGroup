ef381f6d7f500c2b708b1ed4185ae909981f73cc
rwg <rwgreene@MistyMountainGroup.com>
2025-07-09 18:20:45 -0400

    CLI adding query parms for direct access

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,14 @@
 
 const message: string = "Hello, TypeScript! 8.a";
 console.log(message);
+goPersonal();
 
 
-goPersonal()
+
+
 
 function writeStarted() {
-    console.log('writeStarted');
+  console.log('writeStarted');
 }
 
 
@@ -16,107 +18,146 @@ function writeStarted() {
 function showExtended(divTag: string) {
   const lorem = document.getElementById(divTag);
   if (lorem) {
-      lorem.style.display = lorem.style.display === "block" ? "none" : "block";
+    lorem.style.display = lorem.style.display === "block" ? "none" : "block";
   }
 }
 
 
 function showMainContent(sectionId: string): void {
-// Hide all content sections
-const sections = document.getElementsByClassName('content-section');
-Array.from(sections).forEach((section) => section.classList.remove('active'));
-// Show the selected content section
-const selectedSection = document.getElementById(sectionId);
-if (selectedSection) {
+  // Hide all content sections
+  const sections = document.getElementsByClassName('content-section');
+  Array.from(sections).forEach((section) => section.classList.remove('active'));
+  // Show the selected content section
+  const selectedSection = document.getElementById(sectionId);
+  if (selectedSection) {
     selectedSection.classList.add('active');
-}
+  }
 }
 
 function isDOMReady(): boolean {
-    return document.readyState === 'interactive' || document.readyState === 'complete';
+  return document.readyState === 'interactive' || document.readyState === 'complete';
 }
 
 
-document.addEventListener('DOMContentLoaded', () => {  
+// function preloadAndaddMenuClickHandler(linkID: string, divID: string, htmlFile: string) {
+//   // failed atttempt 
+//   showContents(linkID, divID, htmlFile);
+//   addMenuClickHandler(linkID, divID, htmlFile);
+
+// }
+
+
+document.addEventListener('DOMContentLoaded', () => {
   addMenuClickHandler('interestingLink', 'interesting', 'interesting.html');
-    addMenuClickHandler('scamsLink', 'scams', 'scams.html');
-    addMenuClickHandler('internetLink', 'internet', 'internet.html');
-    addMenuClickHandler('windowsLink', 'windows', 'windows.html');
-    addMenuClickHandler('linuxLink', 'linux', 'whylinux.html');
-    addMenuClickHandler('aboutLink', 'about', 'about.html');
-    
-  }); 
+  addMenuClickHandler('windowsLink', 'windows', 'windows.html');
+  addMenuClickHandler('linuxLink', 'linux', 'whylinux.html');
+  addMenuClickHandler('aboutLink', 'about', 'about.html');
+  addMenuClickHandler('internetLink', 'internet', 'internet.html');
+  addMenuClickHandler('scamsLink', 'scams', 'scams.html');
+  handleURLQuery();
+
+});
 
 
+function handleURLQuery() {
+  // const queryParams = new URLSearchParams(window.location.search);
+  const path = window.location.pathname;
+  // const queryParams = new URLSearchParams(path.substring(path.indexOf('?') + 1));
+  const queryParams = new URLSearchParams(window.location.search);
+  const segments = path.split('/').filter(Boolean);
 
+  for (const [key, value] of queryParams.entries()) {
 
-  function addMenuClickHandler(linkID: string, divID: string, htmlFile: string): void {
-    const theLink: HTMLElement | null = document.getElementById(linkID);
-    const theContent: HTMLElement | null = document.getElementById(divID);
-  
-    if (theLink && theContent) {
-  
-      console.log('gonna load'); 
-  
-      theLink.addEventListener('click', (e: Event) => {
-        e.preventDefault();
-        showContents(linkID, divID, htmlFile);
-  
-        // fetch('dist/' + htmlFile)
-        //   .then((response: Response) => {
-        //     if (!response.ok) {
-        //       throw new Error('Network response was not ok');
-        //     }
-        //     return response.text();
-        //   })
-        //   .then((data: string) => {
-  
-        //     console.log(`got data len=${data.length}`);
-  
-        //     theContent.innerHTML = data;
-        //     console.log(`loaded`);
-  
-        //   })
-        //   .catch((error: Error) => {
-        //     theContent.innerHTML = `Error loading content: ${error.message}`;
-        //   });
-      });
-
-    } else {
-      console.error(`Required DOM elements ${theLink}/ ${theLink} or ${theContent} not found`);
-    } 
+    if (key === 'scams') {
+      document.getElementById('scamsLink')?.click();
+    }
+    if (key === 'interesting') {
+      document.getElementById('interestingLink')?.click();
+    }
+    if (key === 'internet') {
+      document.getElementById('internetLink')?.click();
+    }
+    if (key === 'windows') {
+      document.getElementById('windowsLink')?.click();
+    }
+    if (key === 'linux') {
+      document.getElementById('linuxLink')?.click();
+    }
+    if (key === 'about') {
+      document.getElementById('aboutLink')?.click();
+    }
   }
 
 
+}
 
-  function showContents(linkID: string, divID: string, htmlFile: string) {
-    console.log('showContents', linkID, divID, htmlFile);
-     
+
+function addMenuClickHandler(linkID: string, divID: string, htmlFile: string): void {
   const theLink: HTMLElement | null = document.getElementById(linkID);
   const theContent: HTMLElement | null = document.getElementById(divID);
 
   if (theLink && theContent) {
 
-    console.log('gonna load'); 
-    
-    fetch("dist/" + htmlFile + "?t=" + Date.now())
-    .then((response: Response) => {
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-      return response.text();
-    })
-    .then((data: string) => {
+    theLink.addEventListener('click', (e: Event) => {
+      e.preventDefault();
+      showContents(linkID, divID, htmlFile);
 
-      console.log(`got data len=${data.length}`);
 
-      theContent.innerHTML = data;
-      console.log(`loaded`);
+      // fetch('dist/' + htmlFile)
+      //   .then((response: Response) => {
+      //     if (!response.ok) {
+      //       throw new Error('Network response was not ok');
+      //     }
+      //     return response.text();
+      //   })
+      //   .then((data: string) => {
 
-    })
-    .catch((error: Error) => {
-      theContent.innerHTML = `Error loading content: ${error.message}`;
+      //     console.log(`got data len=${data.length}`);
+
+      //     theContent.innerHTML = data;
+      //     console.log(`loaded`);
+
+      //   })
+      //   .catch((error: Error) => {
+      //     theContent.innerHTML = `Error loading content: ${error.message}`;
+      //   });
     });
+
+  } else {
+    console.error(`Required DOM elements ${theLink}/ ${theLink} or ${theContent} not found`);
+  }
+}
+
+
+
+function showContents(linkID: string, divID: string, htmlFile: string) {
+  console.log('showContents', linkID, divID, htmlFile);
+
+  const theLink: HTMLElement | null = document.getElementById(linkID);
+  const theContent: HTMLElement | null = document.getElementById(divID);
+
+  if (theLink && theContent) {
+
+    console.log(`gonna load divID: ${divID} file: ${htmlFile}`);
+
+    fetch("dist/" + htmlFile + "?t=" + Date.now())
+      .then((response: Response) => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.text();
+      })
+      .then((data: string) => {
+
+        console.log(`got data len=${data.length}`);
+
+        theContent.innerHTML = data;
+        console.log(`loaded`);
+
+      })
+      .catch((error: Error) => {
+        theContent.innerHTML = `Error loading content: ${error.message}`;
+      });
 
     // theLink.addEventListener('click', (e: Event) => {
     //   e.preventDefault();
@@ -146,84 +187,84 @@ document.addEventListener('DOMContentLoaded', () => {
 }
 
 
-  
-  function goSafety() {
 
-    const personalStuffElements = document.querySelectorAll('.personalStuff') as NodeListOf<HTMLElement>;
-    const internetSafetyStuffElements = document.querySelectorAll('.internetSafetyStuff') as NodeListOf<HTMLElement>;
+function goSafety() {
 
-    personalStuffElements.forEach((element) => {
-      element.style.display = 'none';
-    });
-    internetSafetyStuffElements.forEach((element) => {
-      element.style.display = 'initial';
-    });
+  const personalStuffElements = document.querySelectorAll('.personalStuff') as NodeListOf<HTMLElement>;
+  const internetSafetyStuffElements = document.querySelectorAll('.internetSafetyStuff') as NodeListOf<HTMLElement>;
 
-  }
+  personalStuffElements.forEach((element) => {
+    element.style.display = 'none';
+  });
+  internetSafetyStuffElements.forEach((element) => {
+    element.style.display = 'initial';
+  });
 
-  function goPersonal() {
+}
 
-    const personalStuffElements = document.querySelectorAll('.personalStuff') as NodeListOf<HTMLElement>;
-    const internetSafetyStuffElements = document.querySelectorAll('.internetSafetyStuff') as NodeListOf<HTMLElement>;
+function goPersonal() {
 
-    personalStuffElements.forEach((element) => {
-      element.style.display = 'initial';
-    });
-    internetSafetyStuffElements.forEach((element) => {
-      element.style.display = 'none';
-    });
+  const personalStuffElements = document.querySelectorAll('.personalStuff') as NodeListOf<HTMLElement>;
+  const internetSafetyStuffElements = document.querySelectorAll('.internetSafetyStuff') as NodeListOf<HTMLElement>;
 
-  }
+  personalStuffElements.forEach((element) => {
+    element.style.display = 'initial';
+  });
+  internetSafetyStuffElements.forEach((element) => {
+    element.style.display = 'none';
+  });
 
-
+}
 
 
-  function loadQuotes(): Promise<any> {
-    return fetch('dist/data/quotes.json')
-        .then(response => response.json());
+
+
+function loadQuotes(): Promise<any> {
+  return fetch('dist/data/quotes.json')
+    .then(response => response.json());
 }
 
 
 function getRandomQuote(): Promise<any> {
   return loadQuotes().then(quotes => {
-      const randomIndex = Math.floor(Math.random() * quotes.length);
-      return quotes[randomIndex];
+    const randomIndex = Math.floor(Math.random() * quotes.length);
+    return quotes[randomIndex];
   });
 }
 
 function getRandomQuoteFromServer(): Promise<any> {
-  
+
   return fetch('http://localhost:3000/quote')
-      .then(response => response.json());
-  };
+    .then(response => response.json());
+};
 
 
 
 
-  function getRandomQuoteAndUpdateTextArea() {
-    getRandomQuote().then(quote => {
-        const output = document.getElementById('quoteOutput') as HTMLParagraphElement;
-        const cite = document.getElementById('quoteCite'); 
-        output.innerHTML = quote.Comment;
-        if ( quote.Author.length > 0 ) {
-          cite.textContent = `Citation: ${quote.Author}`;
-        } else {
-          cite.textContent = '';
-        }
-        // document.getElementById('quoteOutput').innerHTML = `${quote.Comment}\n-- ${quote.Author}`;
-    });
+function getRandomQuoteAndUpdateTextArea() {
+  getRandomQuote().then(quote => {
+    const output = document.getElementById('quoteOutput') as HTMLParagraphElement;
+    const cite = document.getElementById('quoteCite');
+    output.innerHTML = quote.Comment;
+    if (quote.Author.length > 0) {
+      cite!.textContent = `Citation: ${quote.Author}`;
+    } else {
+      cite!.textContent = '';
+    }
+    // document.getElementById('quoteOutput').innerHTML = `${quote.Comment}\n-- ${quote.Author}`;
+  });
 }
 
-  function changeGetQuoteWords() {
-    console.log('changeGetQuoteWords');
-    const getQuoteButton = document.getElementById('getQuote') as HTMLButtonElement;
-    getQuoteButton.textContent = 'Get Another Quote';
-  }
+function changeGetQuoteWords() {
+  console.log('changeGetQuoteWords');
+  const getQuoteButton = document.getElementById('getQuote') as HTMLButtonElement;
+  getQuoteButton.textContent = 'Get Another Quote';
+}
 
 
 
-//   Attempt to capture any click and close the open extended sections 
-//   BUT it runs after I clicko on Extended and therefore it closes what I open 
+//   Attempt to capture any click and close the open extended sections
+//   BUT it runs after I clicko on Extended and therefore it closes what I open
 //   function handleClicksCloseExtended(event: MouseEvent): void {
 //     // Get all the extended content elements
 //     const extendedContentElements = document.querySelectorAll('.extendedComment');
@@ -231,7 +272,7 @@ function getRandomQuoteFromServer(): Promise<any> {
 //     extendedContentElements.forEach((element) => {
 
 // |||
-//       const theDiv = document.getElementById(element.id); 
+//       const theDiv = document.getElementById(element.id);
 // //      theDiv.style.display = "none";
 
 
@@ -361,5 +402,5 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 
-*/ 
+*/
 
